d118bdf51a10edf136c1c85bd8b6aef703d27511
atangmac <atangmac@gmail.com>
2015-06-22 19:00:24 +0200

    commit android

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,19 @@
+
+var app = angular.module('MyFutureFortuneApp', ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider
+      .when('/Book', {
+        templateUrl: 'partials/book.html'
+//        controller: 'BookCtrl',
+//        controllerAs: 'book'
+      })
+      .when('/Book/:bookId/ch/:chapterId', {
+        templateUrl: 'chapter.html',
+        controller: 'ChapterCtrl',
+        controllerAs: 'chapter'
+      });
+
+//    $locationProvider.html5Mode(true);
+}])
