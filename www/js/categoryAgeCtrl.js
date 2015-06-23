e3dc4616c8632dd167283e26b8e80ce159f791ab
atangmac <atangmac@gmail.com>
2015-06-23 12:15:53 +0200

    bla categoryAge transition

--- www/js/categoryAgeCtrl.js
+++ www/js/categoryAgeCtrl.js
@@ -1,7 +1,7 @@
-app.controller('CategoryAgeCtrl', ['$scope', '$location', function ($scope, $location) {
+app.controller('CategoryAgeCtrl', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
 	$rootScope.showFavorite = false;
 
 	$scope.selectCategoryAge = function() {
-		$location.path('plateau');
+		$location.path('questionWhenTax');
 	};
 }]);
