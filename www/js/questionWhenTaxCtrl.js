d0e7314578832fa89de8045bb39c2ca426df74fb
atangmac <atangmac@gmail.com>
2015-06-23 09:55:35 +0200

    panel

--- www/js/questionWhenTaxCtrl.js
+++ www/js/questionWhenTaxCtrl.js
@@ -1,5 +1,12 @@
-app.controller('CategoryAgeCtrl', ['$scope', function ($scope) {
-	$scope.selectCategoryAge = function() {
-		alert("you selected this item");
-	};
+app.controller('QuestionWhenTaxCtrl', ['$scope', function ($scope) {
+	$scope.answered = false;
+	$scope.ok = false;
+	
+	$scope.answer = function(a) {
+		if (a) {
+			$scope.addPoint(100);
+		}
+		$scope.answered = true;
+		$scope.ok = a;
+	}
 }]);
