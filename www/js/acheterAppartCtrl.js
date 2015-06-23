a912b876fcecc4dbd06204f0665da938094631d0
atangmac <atangmac@gmail.com>
2015-06-23 11:49:11 +0200

    acheterAppart

--- www/js/acheterAppartCtrl.js
+++ www/js/acheterAppartCtrl.js
@@ -1,8 +1,12 @@
 app.controller('AcheterAppartCtrl', ['$scope', function ($scope) {
 	$scope.initFavorite('Acheter son appartement');
-	
+	$scope.finalResult = false;
 	$scope.answer = function(b) {
 		$scope.answered = true;
 		$scope.ok = b;
+	}
+	
+	$scope.compute = function() {
+		$scope.finalResult = true;
 	}
 }]);
