19d9256682c4ce5da71d4b536a456e5cf658fb24
DHKold <dhkold@gmail.com>
2015-06-24 11:22:35 +0200

    Scenario Coherence

--- www/js/services/Scenario.js
+++ www/js/services/Scenario.js
@@ -6,10 +6,10 @@ app.service('ScenarioSvc', function(){
 		init : function(){
 			this.currentStep = 0;
 			this.actions.push({ screen : 'questionWhenTax', image:'tax', title:'Imp&ocirc;ts', dice:5 });
-			this.actions.push({ screen : 'PEA', image: 'quizz', title: 'Quizz'});
-			this.actions.push({ screen : 'acheterAppart', image:'house', title:'Immobilier', dice:4 });
-			this.actions.push({ screen : 'immo' });
-			this.actions.push({ screen : 'warren' });
+			this.actions.push({ screen : 'PEA', image: 'quizz', title: 'Quizz<br />PEA', dice:2 });
+			this.actions.push({ screen : 'acheterAppart', image:'house', title:'Immobilier', dice:3 });
+			this.actions.push({ screen : 'immo', image:'house', title:'Immobilier', dice:5 });
+			this.actions.push({ screen : 'warren', image:'quote', title:'Citation', dice:2 });
 			this.actions.push({ handler : function(){ alert('Step 3'); } });
 			this.actions.push({ handler : function(){ alert('Step 4'); } });
 			this.actions.push({ handler : function(){ alert('Step 5'); } });
